f0467e12b9e1c1b9905d2438aaa46af3d6943aea
Aiden Appleby <aiden.appleby3@gmail.com>
2025-07-03 18:45:26 -0400

    folder loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+
+import { fetchApi } from "@/tools/axios.tools";
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [folders, setFolders] = useState<null | string[]>(null);
+
+  const listFolders = async (): Promise<string[] | null> => {
+    const response = await fetchApi<string[]>({
+      url: "/aplb/folders",
+      method: "GET",
+    });
+    console.log("Folders:", response);
+    if (response.success) {
+      return response.data;
+    } else {
+      return null;
+    }
+  };
+
+  const initialize = async () => {
+    const folders = await listFolders();
+    setFolders(folders);
+  };
+
+  useEffect(() => {
+    // Initialize the page
+    initialize();
+  }, []);
+
   return (
     <div>
       <h1>Welcome to OpenBucket</h1>
+      {folders ? (
+        folders.map((folder, index) => (
+          <div key={index} className="folder-item">
+            <h2>{folder}</h2>
+          </div>
+        ))
+      ) : (
+        <p>Loading folders...</p>
+      )}
     </div>
   );
 };
